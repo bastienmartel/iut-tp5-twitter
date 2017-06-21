8f24b64b347ae65687712470ec9da82457ea240d
MARTEL BASTIEN p1404247 <p1404247@univ-lyon1.fr>
2017-06-21 11:39:43 +0200

    Etape 5 - Création d'un composant

---
+++ frontend/src/components/Tweet.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Tweet">
+    <h1> Bienvenue sur la Tweetosphère ! </h1>
+    <div {{ tweet.auteur }} : {{ tweet.tweet }} </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tweet',
+  props: [{auteur: 'Drump', tweet: 'Covfefe'}]
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
