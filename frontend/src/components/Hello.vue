2be6d0a9155719c881cbab4c17f4275189128003
MARTEL BASTIEN p1404247 <p1404247@univ-lyon1.fr>
2017-06-21 10:42:22 +0200

    Etape 2 - Routeur

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <ul>
+      <li><router-link to="/timeline">Timeline</router-link></li>
+    </ul>
   </div>
 </template>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/timeline',
+      name: 'Hello',
+      component: Timeline
     }
   ]
 })
