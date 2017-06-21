3b4ea15bf7f4c4a2459cb4efb474367556b03485
MARTEL BASTIEN p1404247 <p1404247@univ-lyon1.fr>
2017-06-21 10:53:29 +0200

    Etape 3 - Ajout de données

---
+++ frontend/src/components/Timeline.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Timeline">
+    <h1>{{ tweets }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+  data () {
+    return {
+      tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Timeline from '@/components/Timeline'
 
 Vue.use(Router)
 
@@ -13,7 +14,7 @@ export default new Router({
     },
     {
       path: '/timeline',
-      name: 'Hello',
+      name: 'Timeline',
       component: Timeline
     }
   ]
